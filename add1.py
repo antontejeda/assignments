f974ff328ecc5bf0bae7e360d2d4fcdfc3c9bb40
Anton Tejeda <edt371@student@hks.harvard.edu>
2018-01-05 14:23:28 -0500

    add day 1 homework

---
+++ add1.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+import sys
+name = sys.stdin.read()
+print int(name) + 1
---
+++ hello.py
@@ -0,0 +1,10 @@
+print "Hello, im in a python program"
+
+a=1
+b=2
+print "a + b is"
+print a+b
+x = "Anton"
+print "my name is " + x
+
+
