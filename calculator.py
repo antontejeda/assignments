10b2c0aba23500b45e69ce6d5ad665c6440f44a0
Anton Tejeda <edt371@student@hks.harvard.edu>
2018-01-07 19:37:58 -0500

    add functions to calculator

---
+++ calculator.py
@@ -0,0 +1,19 @@
+def multiply(a,b):
+	return a * b
+
+x = multiply (5,5)
+
+def add(a,b):
+	return a + b
+
+x = add (5,5)
+
+def subtract(a,b):
+	return a - b
+
+x = subtract(5,5)
+
+def divide(a,b):
+	return a / b
+
+x = divide (5,5)
