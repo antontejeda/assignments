1e5bb1478c5d50bc2230852a59cf76d9c1049ba7
Anton Tejeda <edt371@student@hks.harvard.edu>
2018-01-07 19:59:19 -0500

    add functions to calculator

---
+++ calculator.js
@@ -0,0 +1,25 @@
+function multiply(a,b) {
+	return a * b
+}
+
+var x = multiply (5,5)
+
+function add(a,b){
+	return a + b
+}
+
+var x = add (5,5)
+
+function subtract(a,b) {
+	return a - b
+}
+
+
+var x = subtract(5,5)
+
+function divide(a,b) {
+	return a / b
+}
+
+
+var x = divide (5,5)
