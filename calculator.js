09a3849fd125ee113bbfb123835d04e95c233a41
Anton Tejeda <edt371@student@hks.harvard.edu>
2018-01-07 20:15:27 -0500

    fixing java script second time

--- calculator.js
+++ calculator.js
@@ -30,6 +30,7 @@ var x = divide(5,6)
 // This prints x to the terminal for humans to read
 console.log("I'm going use the calculator functions to multiply 5 and 6")
 var x = multiply(5,6)
+console.log(x)
 
 
 
