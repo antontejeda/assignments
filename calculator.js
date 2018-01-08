9eb9c442533c1082012b9329dd6bafd9003eec3b
Anton Tejeda <edt371@student@hks.harvard.edu>
2018-01-07 20:12:42 -0500

    fixing java script

--- calculator.js
+++ calculator.js
@@ -1,29 +1,35 @@
-function multiply(a,b) {
-	return a * b
+// This part of the code defines a function
+function multiply(a,b) { 
+	return (a * b)
 }
 
-var x = multiply (5,5)
+// This part of the code then calls that function and assigns the variable x to its return value
+var x = multiply(5,6)
 
-function add(a,b){
-	return a + b
+function add(a,b) { 
+	return (a + b)
 }
 
-var x = add (5,5)
+// This part of the code then calls that function and assigns the variable x to its return value
+var x = add(5,6)
 
-function subtract(a,b) {
-	return a - b
+function subtract(a,b) { 
+	return (a - b)
 }
 
+// This part of the code then calls that function and assigns the variable x to its return value
+var x = subtract(5,6)
 
-var x = subtract(5,5)
-
-function divide(a,b) {
-	return a / b
+function divide(a,b) { 
+	return (a / b)
 }
 
+// This part of the code then calls that function and assigns the variable x to its return value
+var x = divide(5,6)
 
-var x = divide (5,5)
+// This prints x to the terminal for humans to read
+console.log("I'm going use the calculator functions to multiply 5 and 6")
+var x = multiply(5,6)
 
-console.log("I'm going use the calculator functions to multiply 5 and 5")
-var x = multiply(5,5)
-console.log(x)
+
+
